da3a2f8cba10f47df2e2f742c055acbfda3460b8
er-ri <724chen@gmail.com>
2023-08-14 17:21:17 +0900

    feat: spike prime -> buildhat

--- vehicle/app.py
+++ vehicle/app.py
@@ -3,28 +3,35 @@ The module runs on native Raspberry PI environment.
 Using the following command to launch the flask server:
     flask run --host=0.0.0.0
 """
-import serial
+import json
 from flask import Flask
-from flask_socketio import SocketIO 
+from flask_socketio import SocketIO
 
-serialPort = serial.Serial(
-    port='/dev/ttyAMA1',
-    baudrate = 115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
-)
+from buildhat import MotorPair, ColorSensor
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+pair = MotorPair('A', 'B')
+dist = ColorSensor('C')
+
 @socketio.on('json')
-def handle_json(json):
-    global serialPort
-    if serialPort:
-        serialPort.write(json.encode())
-    print('Command Received: ' + str(json))
+def handle_json(command):
+    parameters = json.loads(command)
+    speed = parameters["speed"]
+    angle = parameters["angle"]
+
+    if angle == 0:
+        speedl = speed
+        speedr = speed
+    elif angle < 0:
+        speedl = speed * (1+angle)
+        speedr = speed
+    elif angle > 0:
+        speedl = speed
+        speedr = (speed * (1-angle))  
+
+    pair.start(speedl=-speedl, speedr=speedr)
 
 if __name__ == "__main__":
     socketio.run(app)
